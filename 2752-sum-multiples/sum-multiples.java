3393d8d7f8d5a4931eff7185687999542710e246
Abdixxx <hannanmessi1010@gmail.com>
2026-07-22 13:59:33 +0530

    Time: 3 ms (95.48%) | Memory: 42.4 MB (79.48%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -3,7 +3,6 @@ class Solution {
           int sum=0;
         for(int i=1;i<=n;i++){
             if(i%3==0 || i%5==0 || i%7==0)sum+=i;
-        }return sum;
-        
+        }return sum;  
     }
 }
